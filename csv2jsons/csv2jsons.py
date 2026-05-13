8acec58082f08c6e518723199c5f214f58842606
Eitan <2028969+eitanl@users.noreply.github.com>
2026-05-13 17:39:11 +0300

    consistent order of links in the output JSONS

--- csv2jsons/csv2jsons.py
+++ csv2jsons/csv2jsons.py
@@ -8,10 +8,10 @@ def generate_factions(csv_filename, target_party):
     os.makedirs(output_dir, exist_ok=True)
     
     # Define which CSV columns belong inside the nested "links" object
-    link_columns = {'website', 'facebook', 'twitter', 'instagram', 'telegram', 'whatsapp', 'wikipedia'}
+    link_columns = ['website', 'facebook', 'twitter', 'instagram', 'telegram', 'whatsapp', 'wikipedia']
     
     # Define keys that are handled by this code explicitly (so we don't duplicate them in the generic copy)
-    explicit_keys = {'party', 'logo', 'supporters', 'tags'} | link_columns
+    explicit_keys = {'party', 'logo', 'supporters', 'tags'} | set(link_columns)
 
     with open(csv_filename, mode='r', encoding='utf-8-sig') as csvfile:
         reader = csv.reader(csvfile)
